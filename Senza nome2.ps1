7ed912d8827cca587b957a9fd614f070a1714f75
nessuno74 <nessuno74@gmail.com>
2019-04-18 12:32:15 +0200

    mmm

---
+++ Senza nome2.ps1
@@ -0,0 +1,28 @@
+﻿$metrics = "disk.numberwrite.summation","disk.numberread.summation"
+$start = (Get-Date).AddMinutes(-5)
+$report = @()
+ 
+$vms = Get-VM | where {$_.PowerState -eq "PoweredOn"}
+$stats = Get-Stat -Realtime -Stat $metrics -Entity $vms -Start $start
+$interval = $stats[0].IntervalSecs
+ 
+$lunTab = @{}
+foreach($ds in (Get-Datastore -VM $vms | where {$_.Type -eq "VMFS"})){
+	$ds.ExtensionData.Info.Vmfs.Extent | %{
+		$lunTab[$_.DiskName] = $ds.Name
+	}
+}
+ 
+$report = $stats | Group-Object -Property {$_.Entity.Name},Instance | %{
+	New-Object PSObject -Property @{
+		VM = $_.Values[0]
+ 		Disk = $_.Values[1]
+ 		IOPSMax = ($_.Group | `
+ 			Group-Object -Property Timestamp | `
+ 			%{$_.Group[0].Value + $_.Group[1].Value} | `
+ 			Measure-Object -Maximum).Maximum / $interval
+		Datastore = $lunTab[$_.Values[1]]
+	}
+}
+ 
+$report
---
+++ Senza nome4.ps1
@@ -0,0 +1,19 @@
+﻿$Groups = Get-ADGroup -Filter * -SearchBase 'OU=root,DC=facile55,DC=local'
+
+$Results = foreach( $Group in $Groups ){
+
+    Get-ADGroupMember -Identity $Group |  where {$_.objectClass -eq "user"} | foreach {
+
+        [pscustomobject]@{
+
+            GroupName = $Group.Name
+
+            Name = $_.Name
+
+            }
+
+        }
+
+    }
+
+$Results| Export-Csv -Path c:\data\groups1.csv -NoTypeInformation﻿
